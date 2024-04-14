e4c45d8857779a0d7000d08b0a79116ea5d077b2
Paul Ushiro <ushiropaul2007@gmail.com>
2024-04-14 04:16:41 -0300

    estilos de pages. Mobile. Animaciones. SEO, etc

---
+++ js/encarga.js
@@ -0,0 +1,20 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<span class="titulos-servicios">Zartros<span/>',
+        '<span class="titulos-servicios">Ushza<span/>',
+        '<span class="titulos-servicios">Kali-zen<span/>',
+        '<span class="titulos-servicios">Nollan<span/>'
+    ],
+    stringsElement: '#cadenas-texto',
+    typeSpeed: 75,
+    startDelay: 2000,
+    backSpeed: 80,
+    smartBackspace: true,
+    backDelay: 2000,
+    shuffle: false,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+})
---
+++ js/servicios.js
@@ -0,0 +1,21 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<span class="titulos-servicios">hacking<span/>',
+        '<span class="titulos-servicios">penntesting<span/>',
+        '<span class="titulos-servicios">spyware<span/>',
+        '<span class="titulos-servicios">Black hacking<span/>',
+        '<span class="titulos-servicios">Red hacking<span/>'
+    ],
+    stringsElement: '#cadenas-texto',
+    typeSpeed: 75,
+    startDelay: 2000,
+    backSpeed: 80,
+    smartBackspace: true,
+    backDelay: 2000,
+    shuffle: false,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+})
